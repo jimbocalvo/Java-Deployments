09dd53976df505b59caeb6e0f70c68eabeeb8213
James <43893957+jimbocalvo@users.noreply.github.com>
2019-12-06 12:00:46 +0000

    Add files via upload

---
+++ Deploy-Application.ps1
@@ -0,0 +1,230 @@
+﻿<#
+.SYNOPSIS
+	This script performs the installation or uninstallation of an application(s).
+.DESCRIPTION
+	The script is provided as a template to perform an install or uninstall of an application(s).
+	The script either performs an "Install" deployment type or an "Uninstall" deployment type.
+	The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+	The script dot-sources the AppDeployToolkitMain.ps1 script which contains the logic and functions required to install or uninstall an application.
+.PARAMETER DeploymentType
+	The type of deployment to perform. Default is: Install.
+.PARAMETER DeployMode
+	Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
+.PARAMETER AllowRebootPassThru
+	Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+.PARAMETER TerminalServerMode
+	Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Destkop Session Hosts/Citrix servers.
+.PARAMETER DisableLogging
+	Disables logging to file for the script. Default is: $false.
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
+.EXAMPLE
+    Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
+.NOTES
+	Toolkit Exit Code Ranges:
+	60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
+	69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
+	70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
+.LINK 
+	http://psappdeploytoolkit.com
+#>
+<#
+	Version History
+	1.0.0 - 27/06/2017 - First working version deployed from SCCM
+	1.0.1 - 06/07/2017 - Copies deployment.config and deployment.properties to C:\Windows\Sun\Java\Deployment
+#>
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Install','Uninstall')]
+	[string]$DeploymentType = 'Install',
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Interactive','Silent','NonInteractive')]
+	[string]$DeployMode = 'Interactive',
+	[Parameter(Mandatory=$false)]
+	[switch]$AllowRebootPassThru = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$TerminalServerMode = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$DisableLogging = $false
+)
+
+Try {
+	## Set the script execution policy for this process
+	Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch {}
+	
+	##*===============================================
+	##* VARIABLE DECLARATION
+	##*===============================================
+	## Variables: Application
+	[string]$appVendor = 'Oracle'
+	[string]$appName = 'Java Runtime Environment'
+	[string]$appVersion = '8'
+	[string]$appArch = ''
+	[string]$appLang = 'EN'
+	[string]$appRevision = '01'
+	[string]$appScriptVersion = '1.0.1'
+	[string]$appScriptDate = '27/06/2017'
+	[string]$appScriptAuthor = 'James Calvert'
+	##*===============================================
+	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
+	[string]$installName = ''
+	[string]$installTitle = ''
+	
+	##* Do not modify section below
+	#region DoNotModify
+	
+	## Variables: Exit Code
+	[int32]$mainExitCode = 0
+	
+	## Variables: Script
+	[string]$deployAppScriptFriendlyName = 'Deploy Application'
+	[version]$deployAppScriptVersion = [version]'3.6.8'
+	[string]$deployAppScriptDate = '02/06/2016'
+	[hashtable]$deployAppScriptParameters = $psBoundParameters
+	
+	## Variables: Environment
+	If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation } Else { $InvocationInfo = $MyInvocation }
+	[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+	
+	## Dot source the required App Deploy Toolkit Functions
+	Try {
+		[string]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
+		If (-not (Test-Path -LiteralPath $moduleAppDeployToolkitMain -PathType 'Leaf')) { Throw "Module does not exist at the specified location [$moduleAppDeployToolkitMain]." }
+		If ($DisableLogging) { . $moduleAppDeployToolkitMain -DisableLogging } Else { . $moduleAppDeployToolkitMain }
+	}
+	Catch {
+		If ($mainExitCode -eq 0){ [int32]$mainExitCode = 60008 }
+		Write-Error -Message "Module [$moduleAppDeployToolkitMain] failed to load: `n$($_.Exception.Message)`n `n$($_.InvocationInfo.PositionMessage)" -ErrorAction 'Continue'
+		## Exit the script, returning the exit code to SCCM
+		If (Test-Path -LiteralPath 'variable:HostInvocation') { $script:ExitCode = $mainExitCode; Exit } Else { Exit $mainExitCode }
+	}
+	
+	#endregion
+	##* Do not modify section above
+	##*===============================================
+	##* END VARIABLE DECLARATION
+	##*===============================================
+		
+	If ($deploymentType -ine 'Uninstall') {
+		##*===============================================
+		##* PRE-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Installation'
+		
+		## Show Welcome Message, verify there is enough disk space to complete the install
+		Show-InstallationWelcome -CheckDiskSpace
+		
+		## Show Progress Message (with the default message)
+		## Show-InstallationProgress
+		
+		## <Perform Pre-Installation tasks here>
+		## Show-InstallationProgress -StatusMessage 'Removing Older Java Runtime Environments'
+		## Execute-MSI -Action 'Uninstall' -Path '{26A24AE4-039D-4CA4-87B4-2F83216017FF}' -Parameters '/passive' #6u17 32bit
+		## Execute-MSI -Action 'Uninstall' -Path '{26A24AE4-039D-4CA4-87B4-2F86416017FF}' -Parameters '/passive' #6u17 64bit
+		## Execute-MSI -Action 'Uninstall' -Path '{26A24AE4-039D-4CA4-87B4-2F83216039FF}' -Parameters '/passive' #6u39 32bit
+		## Execute-MSI -Action 'Uninstall' -Path '{26A24AE4-039D-4CA4-87B4-2F86416039FF}' -Parameters '/passive' #6u39 64bit
+		
+		##*===============================================
+		##* INSTALLATION 
+		##*===============================================
+		[string]$installPhase = 'Installation'
+		
+		## Handle Zero-Config MSI Installations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) { $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ } }
+		}
+		
+		## <Perform Installation tasks here>
+        Show-InstallationProgress -StatusMessage 'Copying Config Files'
+		Copy-File -Path "$dirFiles\install.cfg" -Destination "$envWindir\install.cfg"
+		New-Folder -Path "$envWinDir\Sun\Java\Deployment"
+		Copy-File -Path "$dirFiles\deployment.config" -Destination "$envWindir\Sun\Java\Deployment\deployment.config"
+		Copy-File -Path "$dirFiles\deployment.properties" -Destination "$envWindir\Sun\Java\Deployment\deployment.properties"
+		Start-Sleep 1
+        Show-InstallationProgress -StatusMessage 'Installing Java Runtime Environment 8u74'
+		Execute-Process -Path "jre-8u74-windows-i586.exe" -Parameters "INSTALLCFG=C:\Windows\install.cfg"
+		Show-InstallationProgress -StatusMessage 'Setting Environment Variable'
+		[Environment]::SetEnvironmentVariable("deployment.expiration.check.enabled", "false", "Machine")
+		Start-Sleep 1
+		##deployment.expiration.check.enabled false
+		
+		
+		##Show-InstallationProgress -StatusMessage 'Setting Registry Keys'
+		##Remove-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'SunJavaUpdateSched'
+		##Set-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Update\Policy' -Name 'EnableAutoUpdateCheck' -Value '0' -Type 'DWord'
+		##Set-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Update\Policy' -Name 'EnableJavaUpdate' -Value '0' -Type 'DWord'
+		##Set-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Update\Policy' -Name 'NotifyDownload' -Value '0' -Type 'DWord'
+		##Set-RegistryKey -Key 'HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Update\Policy' -Name 'NotifyInstall' -Value '0' -Type 'DWord'
+
+
+		##*===============================================
+		##* POST-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Installation'
+		
+		## <Perform Post-Installation tasks here>
+		
+		## Display a message at the end of the install
+		If (-not $useDefaultMsi) {}
+	}
+	ElseIf ($deploymentType -ieq 'Uninstall')
+	{
+		##*===============================================
+		##* PRE-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Uninstallation'
+		
+		## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+		Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+		
+		## Show Progress Message (with the default message)
+		Show-InstallationProgress
+		
+		## <Perform Pre-Uninstallation tasks here>
+		
+		
+		##*===============================================
+		##* UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Uninstallation'
+		
+		## Handle Zero-Config MSI Uninstallations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat
+		}
+		
+		# <Perform Uninstallation tasks here>
+		Show-InstallationProgress -StatusMessage 'Removing Java Runtime Environment 7'
+		Execute-MSI -Action Uninstall -Path '{26A24AE4-039D-4CA4-87B4-2F83217000FF}' -Parameters '/passive' #32bit
+
+		##*===============================================
+		##* POST-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Uninstallation'
+		
+		## <Perform Post-Uninstallation tasks here>
+		
+		
+	}
+	
+	##*===============================================
+	##* END SCRIPT BODY
+	##*===============================================
+	
+	## Call the Exit-Script function to perform final cleanup operations
+	Exit-Script -ExitCode $mainExitCode
+}
+Catch {
+	[int32]$mainExitCode = 60001
+	[string]$mainErrorMessage = "$(Resolve-Error)"
+	Write-Log -Message $mainErrorMessage -Severity 3 -Source $deployAppScriptFriendlyName
+	Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
+	Exit-Script -ExitCode $mainExitCode
+}
